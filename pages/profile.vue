93fd3c3d0849b3499d93139d472b843ed2b1285a
Arseni Baranov <arseni-baranov@yandex.ru>
2020-01-23 15:22:44 +0500

    arseni test done

--- pages/profile.vue
+++ pages/profile.vue
@@ -7,16 +7,31 @@
         placeholder="Enter country"
       ></b-form-input>
     </b-form-group>
-    <b-btn variant="primary">Save</b-btn>
+    <b-btn variant="primary" @click="setCountry">Save</b-btn>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Page Prifile',
+    name: 'pagePrifile',
+    methods: {
+      setCountry () {
+        this.$store.commit('profile/set_country', this.country)
+        localStorage.country = this.country
+      }
+    },
     data(){
       return {
         country: 'USA'
+      }
+    },
+    beforeRouteLeave (to, from, next) {
+      if (this.country != localStorage.country) {
+        if (confirm('Данные не сохранены, хотите покинуть страницу?')) {
+          next()
+        }
+      } else {
+        next()
       }
     }
   }
--- pages/user.vue
+++ pages/user.vue
@@ -14,18 +14,35 @@
         placeholder="Enter Familia"
       ></b-form-input>
     </b-form-group>
-    <b-btn variant="primary">Save</b-btn>
+    <b-btn variant="primary" @click="saveUser">Save</b-btn>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Page user',
+    name: 'pageUser',
+    methods: {
+      saveUser () {
+        this.$store.commit('user/set_name', this.name)
+        this.$store.commit('user/set_surname', this.surname)	
+        localStorage.name = this.name
+        localStorage.surname = this.surname
+      }
+    },    
     data(){
       return{
         name: '',
         surname:''
       }
-    }
+    },
+    beforeRouteLeave (to, from, next) {
+      if (this.name != localStorage.name || this.surname != localStorage.surname) {
+        if (confirm('Данные не сохранены, хотите покинуть страницу?')) {
+          next()
+        }
+      } else {
+        next()
+      }
+    }    
   }
 </script>
